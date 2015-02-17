125d004c908dd7e73e93ab7f16284822bab7010d
Rein Henrichs <rein.henrichs@gmail.com>
2015-02-17 11:18:04 -0800

    Add tick count and reformat instructions

--- src/Hask8080/Instructions.hs
+++ src/Hask8080/Instructions.hs
@@ -2,51 +2,53 @@
 
 module Hask8080.Instructions where
 
-import           Control.Lens
+import Control.Applicative
+import Control.Lens
 
-import           Hask8080.CPU   (CPU, pc, reg, reg16, regMem)
-import           Hask8080.Types (Address, Byte, Reg (..), Reg16 (..))
+import Hask8080.CPU (CPU, pc, reg, reg16, regMem)
+import Hask8080.Types (Address, Byte, Reg (..), Reg16 (..))
 
-tick n = pc += n
+tick n      = pc += n
 
-mov :: Reg -> Reg -> CPU
-mov r1 M  = (assign (regMem HL) =<< use (reg r1)) >> tick 7
-mov M  r2 = assign (reg r2)    =<< use (regMem HL)
-mov r1 r2 = assign (reg r1)    =<< use (reg r2)
+mov         :: Reg -> Reg -> CPU
+mov r1 M    = tick 7  >> regMem HL <~ use (reg r1)
+mov M  r2   = tick 7  >> reg r2    <~ use (regMem HL)
+mov r1 r2   = tick 5  >> reg r1    <~ use (reg r2)
 
-lxi :: Reg16 -> Address -> CPU
-lxi rp addr = reg16 rp .= addr
+lxi         :: Reg16 -> Address -> CPU
+lxi rp addr = tick 10 >> reg16 rp .= addr
 
-stax :: Reg16 -> CPU
-stax rp = assign (regMem rp) =<< use (reg A)
+stax        :: Reg16 -> CPU
+stax rp     = tick 7  >> regMem rp <~ use (reg A)
 
-inr :: Reg -> CPU
-inr M = regMem HL += 1
-inr r = reg r += 1
+inr         :: Reg -> CPU
+inr M       = tick 10 >> regMem HL += 1
+inr r       = tick 5  >> reg r += 1
 
-inx :: Reg16 -> CPU
-inx rp = reg16 rp += 1
+inx         :: Reg16 -> CPU
+inx rp      = tick 5  >> reg16 rp += 1
 
-dcr :: Reg -> CPU
-dcr M = regMem HL -= 1
-dcr r = reg r -= 1
+dcr         :: Reg -> CPU
+dcr M       = tick 10 >> regMem HL -= 1
+dcr r       = tick 5  >> reg r -= 1
 
-dcx :: Reg16 -> CPU
-dcx rp = reg16 rp += 1
+dcx         :: Reg16 -> CPU
+dcx rp      = tick 5  >> reg16 rp += 1
 
-mvi :: Reg -> Byte -> CPU
-mvi M b = regMem HL .= b
-mvi r b = reg r .= b
+mvi         :: Reg -> Byte -> CPU
+mvi M b     = tick 10 >> regMem HL .= b
+mvi r b     = tick 7  >> reg r .= b
 
-dad :: Reg16 -> CPU
-dad rp = do a <- use (reg16 rp)
-            reg16 HL += a
+ldax        :: Reg16 -> CPU
+ldax rp     = tick 7  >> reg A <~ use (regMem rp)
 
-ldax :: Reg16 -> CPU
-ldax rp = assign (reg A) =<< use (regMem rp)
+nop         :: CPU
+nop         = return ()
 
-nop :: CPU
-nop = return ()
+hlt         :: CPU
+hlt         = error "halt"
 
-hlt :: CPU
-hlt = error "halt"
+dad         :: Reg16 -> CPU
+dad rp      = do tick 10
+                 a <- use (reg16 rp)
+                 reg16 HL += a
